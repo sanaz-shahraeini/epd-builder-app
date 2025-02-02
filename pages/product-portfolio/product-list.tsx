6e61ebf58395f6d5f6ad7715d5a7bc9188b7eb2e
Sanaz <s.shahraeini@gmail.com>
2025-02-02 18:29:01 +0330

    fix bug on session product-list2

--- pages/product-portfolio/product-list.tsx
+++ pages/product-portfolio/product-list.tsx
@@ -1,6 +1,17 @@
 
+import { useSession } from "next-auth/react";
 import ProductList from "@/components/product-portfolio/product-list";
 
 export default function ProductListPage() {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  if (!session) {
+    return <div>You need to be authenticated to view this page.</div>;
+  }
+
   return <ProductList />;
 }
